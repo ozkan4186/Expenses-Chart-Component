1754a800e51199fc585cd8755bb67e5e8c7d8fd3
özkan demir <ozkanndemir2009@gmail.com>
2022-10-05 02:03:25 +0300

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+//GET DATA FROM JSON FILE
+const data = async() => {
+    const res = await fetch("data.json")
+    console.log(res);
+    const data = await res.json()
+    console.log(data);
+   
+  data.map(day,amount)
+  
+
+
+  
+  
+
+  
+  
+}
+
+console.log(data());
