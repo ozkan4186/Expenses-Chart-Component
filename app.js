4c93d7c8182e64471d2be8f9e0ab3e373c249818
özkan demir <ozkanndemir2009@gmail.com>
2022-10-05 19:46:23 +0300

    js update

---
+++ app.js
@@ -0,0 +1,24 @@
+const myDay = document.querySelectorAll(".days_li li");
+const data = async() => {
+    const res = await fetch("data.json")
+    console.log(res);
+    const data = await res.json()
+    console.log(data);
+   
+
+    //   const {day, amount} = data;
+      data.map(({day, amount},i)=>{
+        console.log(`${day} ${amount} ${i}`)
+       console.log( `myDay[${i}]`);
+
+//    console.log(myDay[i]);
+console.log(myDay[i].firstElementChild.style.height =`${(amount*2).toFixed(0)}px`)
+myDay[i].firstElementChild.style.backgroundColor ="red";
+
+
+      
+      })
+      
+}
+
+console.log(data());
